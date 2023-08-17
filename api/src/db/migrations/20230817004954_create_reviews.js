93c5f494f33250bf4e81f5efd2224d57652af2b1
Joshua Phillips <josh.phillips@aparavi.com>
2023-08-16 18:23:18 -0700

    added knex to project to work with postgress

---
+++ api/src/db/migrations/20230817004954_create_reviews.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.raw(`
+        CREATE TABLE IF NOT EXISTS reviews (
+            id SERIAL PRIMARY KEY,
+            title TEXT,
+            authors TEXT,
+            review TEXT,
+            rating REAL,
+            reason TEXT
+        );
+    `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.raw(`
+        DROP TABLE IF EXISTS reviews;
+    `);
+};
